6217c6bfc192ce2af2694d6da9a724ccbe9d752a
urashid <ur@metaapp.com>
2010-03-16 14:18:57 +0500

    TEST - Data from Sister Site Collected - ListPrice - YouPayPrice - Author - Edition - ISBN 10 Print - ISBN 13 Print - ISBN 10 Digital - ISBN 13 Digital

--- sister_url_parsing.php
+++ sister_url_parsing.php
@@ -1,6 +1,30 @@
 <?php
+    include_once("library/simple_html_dom.php");
+    $url = "http://www.cafescribe.com/index.php?option=com_virtuemart&page=shop.product_details&flypage=shop.flypage&isbn13=9780495114789&storeid=670 ";
+    $html = file_get_dom($url);
+    $ListPrice  = $html->find('div[id=bodycenter] table td',0)->children[2]->children[1]->plaintext;
+    $ListPrice = trim($ListPrice);
+    
+    $YouPayPrice  = $html->find('div[id=bodycenter] table td',0)->children[2]->children[6]->plaintext;
+    $YouPayPrice = trim($YouPayPrice);
 
+    $Author  = $html->find('div[id=bodycenter] table td',0)->children[9]->find('tr',0)->children[1]->plaintext;
+    $Author = trim($Author);
 
+    $Edition = $html->find('div[id=bodycenter] table td',0)->children[9]->find('tr',4)->children[1]->plaintext;
+    $Edition = trim($Edition);
 
+    $ISBN_10_Print = $html->find('div[id=bodycenter] table td',0)->children[9]->find('tr',6)->children[1]->plaintext;
+    $ISBN_10_Print = trim($ISBN_10_Print);
 
+    $ISBN_13_Print = $html->find('div[id=bodycenter] table td',0)->children[9]->find('tr',7)->children[1]->plaintext;
+    $ISBN_13_Print = trim($ISBN_13_Print);
+
+    $ISBN_10_Digital = $html->find('div[id=bodycenter] table td',0)->children[9]->find('tr',8)->children[1]->plaintext;
+    $ISBN_10_Digital = trim($ISBN_10_Digital);
+
+    $ISBN_13_Digital = $html->find('div[id=bodycenter] table td',0)->children[9]->find('tr',9)->children[1]->plaintext;
+    $ISBN_13_Digital = trim($ISBN_13_Digital);
+    
+    var_dump(($ISBN_13_Digital));
 ?>
