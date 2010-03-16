f78cea16161c2266f7de6e9e76522dcbb8b92783
urashid <ur@metaapp.com>
2010-03-16 13:40:08 +0500

    TEST - Condition to check Whther Sister URL Exists

--- index1.php
+++ index1.php
@@ -3,7 +3,7 @@ include_once("library/simple_html_dom.php");
     // Url Long books
     $url= "http://www.bkstr.com/webapp/wcs/stores/servlet/CourseMaterialsResultsView?catalogId=10001&categoryId=9604&storeId=10161&langId=-1&programId=562&termId=100014525&divisionDisplayName=Graduate%20School%20of%20Business&departmentDisplayName=BUS&courseDisplayName=GSB%20101&sectionDisplayName=01&demoKey=null&purpose=browse";
     // URL short Books
-    //$url = "http://www.bkstr.com/webapp/wcs/stores/servlet/CourseMaterialsResultsView?catalogId=10001&categoryId=9604&storeId=10161&langId=-1&programId=562&termId=100014525&divisionDisplayName=Stanford&departmentDisplayName=CHEM&courseDisplayName=130&sectionDisplayName=01&demoKey=d&purpose=browse";
+    $url = "http://www.bkstr.com/webapp/wcs/stores/servlet/CourseMaterialsResultsView?catalogId=10001&categoryId=9604&storeId=10161&langId=-1&programId=562&termId=100014525&divisionDisplayName=Stanford&departmentDisplayName=CHEM&courseDisplayName=130&sectionDisplayName=01&demoKey=d&purpose=browse";
 
     $html = file_get_dom($url);
     
@@ -17,7 +17,7 @@ include_once("library/simple_html_dom.php");
              $BookTitle = $ul[0]->children[$i]->find('span[class=wrap]', 0)->plaintext ;
              
              $SisterUrl_Ancher = $ul[0]->children[$i]->find('div[id=field] a', 0);
-             if($SisterUrl_Ancher->innerhtml != ""){                                   // Check if Sister URL is available 
+             if($SisterUrl_Ancher->plaintext != ""){                                   // Check if Sister URL is available
                  $SisterUrl = $SisterUrl_Ancher->getAttribute("href") ;
              }
               echo "$BookTitle <br /> $SisterUrl <br /><br />";
