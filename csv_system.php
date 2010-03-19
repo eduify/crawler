234779d3b68ecae3ce4689f571c6255af0c36f4e
urashid <ur@metaapp.com>
2010-03-19 13:12:52 +0500

    ENHANCEMENTS - Added Support For Amazon (ItemLookUp = ISBN) and (ItemSearch = Keywords)

--- csv_system.php
+++ csv_system.php
@@ -1,36 +1,61 @@
 <?php
 include_once("library/simple_html_dom.php");
 
-Function getAmazonData($SearchPhrase){
-    include_once("library/amazon/aws_signed_request.php");
+Function getAmazonData($SearchPhrase,$RequestType) {
+    include("aws_signed_request.php");
 
     $public_key = "AKIAIRPU52XIPOIZS5OA";
     $private_key = "MQUKYscxHYenmyPApVY9NmCi/9+KDC2FxiBeZmgn";
-    $pxml = aws_signed_request("com", array("Operation"=>"ItemSearch","SearchIndex"=>"Books","Keywords"=>"$SearchPhrase","ResponseGroup"=>"Large"), $public_key, $private_key);
 
+    if($RequestType=="ItemLookup") {
+        $pxml = aws_signed_request("com", array("Operation"=>"ItemLookup","SearchIndex"=>"Books", "ItemId"=>"$SearchPhrase","IdType"=>"ISBN","ResponseGroup"=>"Large"), $public_key, $private_key);
+        if ($pxml === False) {
+            return false;
+            // Problem in accessing AMAZON API
 
-    if ($pxml === False)
-    {
-        return false;
-        // Problem in accessing AMAZON API
+        }else {
 
+            if($pxml->Items->Item->ASIN == "") {
+
+                return false;
+
+            }else {
+                $Amazon['AmazonListPrice'] = $pxml->Items->Item->ItemAttributes->ListPrice->FormattedPrice;
+                $Amazon['NonAmazonNewPrice'] = $pxml->Items->Item->OfferSummary->LowestNewPrice->FormattedPrice;
+                $Amazon['NonAmazonUsedPrice'] = $pxml->Items->Item->OfferSummary->LowestUsedPrice->FormattedPrice;
+                $Amazon['AmazonDiscountPrice'] = $pxml->Items->Item->Offers->Offer->OfferListing->Price->FormattedPrice;
+                $Amazon['AmazonDetailPageURL'] = $pxml->Items->Item->DetailPageURL;
+                return $Amazon;
+            } // Else
+        }// Else
+
+    }else if($RequestType=="ItemSearch") {
+        $pxml = aws_signed_request("com", array("Operation"=>"ItemSearch","SearchIndex"=>"Books","Keywords"=>"$SearchPhrase","ResponseGroup"=>"Large"), $public_key, $private_key);
+
+        if ($pxml === False) {
+            return false;
+            // Problem in accessing AMAZON API
+
+        }else {
+
+            if($pxml->Items->Item->ItemAttributes->ListPrice->FormattedPrice == "") {
+
+                return false;
+
+            }else {
+                $Amazon['AmazonListPrice'] = $pxml->Items->Item->ItemAttributes->ListPrice->FormattedPrice;
+                $Amazon['NonAmazonNewPrice'] = $pxml->Items->Item->OfferSummary->LowestNewPrice->FormattedPrice;
+                $Amazon['NonAmazonUsedPrice'] = $pxml->Items->Item->OfferSummary->LowestUsedPrice->FormattedPrice;
+                $Amazon['AmazonDiscountPrice'] = $pxml->Items->Item->Offers->Offer->OfferListing->Price->FormattedPrice;
+                $Amazon['AmazonDetailPageURL'] = $pxml->Items->Item->DetailPageURL;
+                return $Amazon;
+            } // Else
+        }// Else
     }
-   else
-   {
-    if($pxml->Items->Item->ItemAttributes->ListPrice->FormattedPrice == ""){
 
-        return false;
 
-    }else {
-      $Amazon['AmazonListPrice'] = $pxml->Items->Item->ItemAttributes->ListPrice->FormattedPrice;
-      $Amazon['NonAmazonNewPrice'] = $pxml->Items->Item->OfferSummary->LowestNewPrice->FormattedPrice;
-      $Amazon['NonAmazonUsedPrice'] = $pxml->Items->Item->OfferSummary->LowestUsedPrice->FormattedPrice;
-      $Amazon['AmazonDiscountPrice'] = $pxml->Items->Item->Offers->Offer->OfferListing->Price->FormattedPrice;
-      $Amazon['AmazonDetailPageURL'] = $pxml->Items->Item->DetailPageURL;
-      
-      return $Amazon;
-    } // Else
-   }// Else
+
+
 
 }
 
--- library/amazon/amazon1.php
+++ library/amazon/amazon1.php
@@ -1,42 +1,69 @@
 <?php
 
-Function getAmazonData($SearchPhrase){
+Function getAmazonData($SearchPhrase,$RequestType) {
     include("aws_signed_request.php");
-   
+
     $public_key = "AKIAIRPU52XIPOIZS5OA";
     $private_key = "MQUKYscxHYenmyPApVY9NmCi/9+KDC2FxiBeZmgn";
-    $pxml = aws_signed_request("com", array("Operation"=>"ItemLookup","ItemId"=>"B000X9FLKM","ResponseGroup"=>"Small"), $public_key, $private_key);
-    $pxml = aws_signed_request("com", array("Operation"=>"ItemSearch","SearchIndex"=>"Books","Keywords"=>"$SearchPhrase","ResponseGroup"=>"Large"), $public_key, $private_key);
 
-    
-   
-    if ($pxml === False)
-    {
-        return false;
-        // Problem in accessing AMAZON API
+    if($RequestType=="ItemLookup") {
+        $pxml = aws_signed_request("com", array("Operation"=>"ItemLookup","SearchIndex"=>"Books", "ItemId"=>"$SearchPhrase","IdType"=>"ISBN","ResponseGroup"=>"Large"), $public_key, $private_key);
+        if ($pxml === False) {
+            return false;
+            // Problem in accessing AMAZON API
 
-    }
-   else
-   {
-    if($pxml->Items->Item->ItemAttributes->ListPrice->FormattedPrice == ""){
-     
-        return false;
+        }else {
+            
+            if($pxml->Items->Item->ASIN == "") {
+
+                return false;
+
+            }else {
+                $Amazon['AmazonListPrice'] = $pxml->Items->Item->ItemAttributes->ListPrice->FormattedPrice;
+                $Amazon['NonAmazonNewPrice'] = $pxml->Items->Item->OfferSummary->LowestNewPrice->FormattedPrice;
+                $Amazon['NonAmazonUsedPrice'] = $pxml->Items->Item->OfferSummary->LowestUsedPrice->FormattedPrice;
+                $Amazon['AmazonDiscountPrice'] = $pxml->Items->Item->Offers->Offer->OfferListing->Price->FormattedPrice;
+                $Amazon['AmazonDetailPageURL'] = $pxml->Items->Item->DetailPageURL;
+                return $Amazon;
+            } // Else
+        }// Else
         
-    }else {
-      $Amazon['AmazonListPrice'] = $pxml->Items->Item->ItemAttributes->ListPrice->FormattedPrice;
-      $Amazon['NonAmazonNewPrice'] = $pxml->Items->Item->OfferSummary->LowestNewPrice->FormattedPrice;
-      $Amazon['NonAmazonUsedPrice'] = $pxml->Items->Item->OfferSummary->LowestUsedPrice->FormattedPrice;
-      $Amazon['AmazonDiscountPrice'] = $pxml->Items->Item->Offers->Offer->OfferListing->Price->FormattedPrice;
-      $Amazon['AmazonDetailPageURL'] = $pxml->Items->Item->DetailPageURL;
-      return $Amazon;
-    } // Else
-   }// Else
+    }else if($RequestType=="ItemSearch") {
+        $pxml = aws_signed_request("com", array("Operation"=>"ItemSearch","SearchIndex"=>"Books","Keywords"=>"$SearchPhrase","ResponseGroup"=>"Large"), $public_key, $private_key);
+
+        if ($pxml === False) {
+            return false;
+            // Problem in accessing AMAZON API
+
+        }else {
+
+            if($pxml->Items->Item->ItemAttributes->ListPrice->FormattedPrice == "") {
+
+                return false;
+
+            }else {
+                $Amazon['AmazonListPrice'] = $pxml->Items->Item->ItemAttributes->ListPrice->FormattedPrice;
+                $Amazon['NonAmazonNewPrice'] = $pxml->Items->Item->OfferSummary->LowestNewPrice->FormattedPrice;
+                $Amazon['NonAmazonUsedPrice'] = $pxml->Items->Item->OfferSummary->LowestUsedPrice->FormattedPrice;
+                $Amazon['AmazonDiscountPrice'] = $pxml->Items->Item->Offers->Offer->OfferListing->Price->FormattedPrice;
+                $Amazon['AmazonDetailPageURL'] = $pxml->Items->Item->DetailPageURL;
+                return $Amazon;
+            } // Else
+        }// Else
+    }
+    
+
+
+
 
 }
 
 
-        $am =  getAmazonData("Guide to Presentations,  Munter, 2nd");
-       var_dump($am);
+//$am =  getAmazonData("Guide to Presentations,  Munter, 2nd","ItemSearch");
+$am =  getAmazonData("9780077219857","ItemLookup");
+
+
+var_dump($am);
 
 
 
